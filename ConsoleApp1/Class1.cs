dcaf708cacc142c63c457ff588d5119ffe6f7065
Prajwol Adhikari <prajwolad18@gmail.com>
2019-01-17 15:08:09 +0545

    Add project files.

---
+++ ConsoleApp1/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class CustomList<T>
+    {
+        public void CList<T>(T data)
+        {
+            List<T> list = new List<T>();
+            list.Add(data);
+            foreach (T i in list)
+            {
+                Console.Write($"{i}\n");
+            }
+
+            //return (T)Convert.ChangeType(data, typeof(T));
+
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomList<T> customList = new CustomList<T>();
+            Console.WriteLine("Please enter any value: ");
+            customList.CList(Convert.ToString(Console.ReadLine()));
+        }
+    }
+       
+}
